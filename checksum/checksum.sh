0803eeed9d356f0ad701247096fc4c2785d34ae6
sudduth <sudduth@uw.edu>
2021-12-07 11:12:01 -0800

    SISENG-1622: Fixed perms.

--- checksum/checksum.sh
+++ checksum/checksum.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #================================================================================================
 # Recursively calculates a checksum for all files in the current directory.
-#
 # To exclude more directories you can add more "-o -type d -name .dirName -prune"s.
 #================================================================================================
 find . -type d -name .git -prune -o -type f -print0 | sort -z | xargs -0 sha1sum | sha1sum | head -c"$@"
