9e7ae97d926e5f950a37d89478e4f19d38c85083
mlpotter <mlpotter@uw.edu>
2022-07-27 15:55:35 -0700

    SISENG-1982: add some comments

--- validateConfigRepo/src/Config.js
+++ validateConfigRepo/src/Config.js
@@ -1,8 +1,17 @@
 'use strict';
 
+/**
+ * "Roughly" represents a config file
+ */
 module.exports = class Config {
     constructor(file, data) {
+        /**
+         * The name of the config file we're looking at.
+         */
         this.file = file;
+        /**
+         * The data found in the config file. Use a yaml parser to convert the raw yaml into a json object.
+         */
         this.data = data;
     }
 }
