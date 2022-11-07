3a89e5b5632322fe0de2dcb579966812b0f7d5c3
mlpotter <mlpotter@uw.edu>
2022-11-07 12:53:21 -0800

    SISENG-2159: testing deploy.sh for deployAppApi action

---
+++ deployAppApi/deploy.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+main() {
+
+# Print out the arguments that were passed in.
+# This helps with debugging these actions
+echo "Script arguments:"
+for a in "$@"; do
+    echo "    $a"
+done
+
+# Parse arguments
+while [[ $# -gt 0 ]]; do
+    arg=$1
+    shift
+    case $arg in
+        --collective ) COLLECTIVE=$1 ; shift ;;
+        --environment ) ENVIRONMENT=$1 ; shift ;;
+        --lambda-function-name ) LAMBDA_FUNCTION_NAME=$1 ; shift ;;
+        --artifact-s3-bucket ) ARTIFACT_S3_BUCKET=$1 ; shift ;;
+        --artifact-s3-key ) ARTIFACT_S3_KEY=$1 ; shift ;;
+        *) fail "Invalid argument: $arg_name"
+    esac 
+done
+
+# Validate arguments
+[[ -z "${COLLECTIVE:-}" ]] && fail "Error: missing argument --collective" 
+[[ -z "${ENVIRONMENT:-}" ]] && fail "Error: missing argument --environment" 
+[[ -z "${LAMBDA_FUNCTION_NAME:-}" ]] && fail "Error: missing argument --lambda-function-name" 
+[[ -z "${ARTIFACT_S3_BUCKET:-}" ]] && fail "Error: missing argument --artifact-s3-bucket" 
+[[ -z "${ARTIFACT_S3_KEY:-}" ]] && fail "Error: missing argument --artifact-s3-key" 
+
+
+# per the terraform definition, the lambda name defaults to the name of the code artifact without the extension
+if [[ "$LAMBDA_FUNCTION_NAME" == "unset" ]]; then
+    artifact=$(basename "$ARTIFACT_S3_KEY")
+    LAMBDA_FUNCTION_NAME="$COLLECTIVE-$ENVIRONMENT-${artifact%.zip}"
+fi
+# echo "::set-output name=lambda_function_name::$LAMBDA_FUNCTION_NAME"
+
+echo "Updating function code..."
+updateResponse=$(
+    aws lambda update-function-code \
+        --publish \
+        --function-name "$LAMBDA_FUNCTION_NAME" \
+        --s3-bucket "$ARTIFACT_S3_BUCKET" \
+        --s3-key    "$ARTIFACT_S3_KEY" \
+)
+echo "Updated."
+
+
+version=$(echo "$updateResponse" | jq -r .Version)
+
+echo >&2 "version: $version" # TODO DELETE ME
+
+echo "Pointing lambda alias 'latest' to function version '$version'"
+aws lambda update-function-code \
+    --function-name "$LAMBDA_FUNCTION_NAME" \
+    --name latest \
+    --version "$version"
+
+}
+
+fail() {
+    echo >&2 "$@"
+    exit 1
+}
+
+main "$@"
