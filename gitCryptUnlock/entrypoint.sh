9b94fc5af530bf33eca4202a67b032a20a9307fe
irenab <irenab@uw.edu>
2024-05-30 15:07:03 -0700

    SISENG-2476 run on older version of Ubuntu

--- gitCryptUnlock/entrypoint.sh
+++ gitCryptUnlock/entrypoint.sh
@@ -8,13 +8,10 @@ echo "$GPG_PRIVATE_KEY" | base64 -d > "$HOME"/git-crypt-key.asc
 gpg --batch --import "$HOME"/git-crypt-key.asc
 
 gpgconf -v --kill gpg-agent
-printf "kill gpg-agent\ n"
 
 gpg-agent -v --daemon --allow-preset-passphrase --max-cache-ttl 3153600000
-printf "start gpg-agent\ n"
 
 /usr/lib/gnupg/gpg-preset-passphrase -v --preset --passphrase "$GPG_KEY_PASS" "$GPG_KEY_GRIP"
-printf "set gpg-preset-passphrase\ n"
 
 git-crypt unlock
 
