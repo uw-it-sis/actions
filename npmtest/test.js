18252f1b437296f30a11e73e26c3273b35c53f14
michaelPotter <mpotter.dev@gmail.com>
2024-07-10 11:38:02 -0700

    test

--- npmtest/test.js
+++ npmtest/test.js
@@ -1,7 +1,10 @@
 const ChildProcess = require('child_process');
 
-let stdout = ChildProcess.execSync('which node');
-console.log(`which node stdout: `, stdout.toString())  // TODO DELETE ME
+const dorun = cmd => {
+    let stdout = ChildProcess.execSync(cmd);
+    console.log(`${cmd} stdout: `, stdout.toString())  // TODO DELETE ME
+}
 
-stdout = ChildProcess.execSync('which npm');
-console.log(`which npm stdout: `, stdout.toString())  // TODO DELETE ME
+dorun('pwd');
+dorun('which node');
+dorun('which npm');
