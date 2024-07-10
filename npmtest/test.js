75177434484295afae90384dc33ad568fd70b017
michaelPotter <mpotter.dev@gmail.com>
2024-07-10 11:41:23 -0700

    test

--- npmtest/test.js
+++ npmtest/test.js
@@ -1,7 +1,7 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+// import { dirname } from 'path';
+// import { fileURLToPath } from 'url';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+// const __dirname = dirname(fileURLToPath(import.meta.url));
 
 console.log(`__dirname: `, __dirname)  // TODO DELETE ME
 
