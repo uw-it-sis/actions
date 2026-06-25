26819f2c0fc23cd0ac259fc7bf3d54e964433ff7
mlpotter <mlpotter@uw.edu>
2026-06-25 13:57:04 -0700

    PLANNING-2029: debugging

--- validateNodeDependencies/src/main.js
+++ validateNodeDependencies/src/main.js
@@ -20,6 +20,7 @@ const CHECKED_PACKAGES = [
 ];
 
 function main() {
+    console.log(`process.cwd(): `, process.cwd())  // TODO DELETE ME
     // Take the repo path as the named action input, the first command line argument, or default to cwd if not given.
     let workingDir = core.getInput("workdir");
     if (!workingDir) {
